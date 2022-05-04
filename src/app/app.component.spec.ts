59f6a62cc6b2992fe5da816b43d82c243a91a25b
ryzhovalex <thed4rkof@gmail.com>
2022-05-04 13:18:29 +0300

    Finish basics

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'ng-testing'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-testing');
+    expect(app.title).withContext("hello").toEqual('ng-testing');
   });
 
   it('should render title', () => {
